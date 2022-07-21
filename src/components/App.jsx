0ee02c440e215c41d973d96c3fd4f3dc62e5e746
Tasha1003Natasha <0955712824n@gmail.com>
2022-07-21 19:02:14 +0300

    Розмітка

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import  {Component } from 'react';
 
 
-// Збірка////
+export class App extends Component { 
+  state = { 
+    contacts: [], 
+    name: "", 
+  }; 
+ 
+  handleChange = (event) => { 
+    const { value, name } = event.target; 
+    this.setState({ [name]: value }); 
+  }; 
+ 
+  render() { 
+    const { contacts, name } = this.state; 
+    console.log({ contacts, name }); 
+ 
+    return ( 
+      <form> 
+        <div className="form"> 
+          <h1 className="form__title">Phonebook</h1> 
+          <label className="form__label"> 
+            <p>Name</p> 
+            <input 
+              value={name} 
+              type="text" 
+              name="name" 
+              className="form__input" 
+              onChange={this.handleChange} 
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$" 
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan" 
+              required 
+            /> 
+          </label> 
+        </div> 
+        <button type="submit" className="form__btn"> 
+          Add contact 
+        </button> 
+ 
+        <h2 className="contact__title">Contacts</h2> 
+        <ul className="contact__list"> 
+          <li className="contact__item">Rosie Simpson: </li> 
+          <li className="contact__item">Hermione Kline: </li> 
+          <li className="contact__item">Eden Clements: </li> 
+        </ul> 
+      </form> 
+    ); 
+  } 
+}
+
+
+ 
+
